23cd02818cba52b22a6ba7ed13e3a2de106a8ae4
arifislamfx <arifislammfx@gmail.com>
2020-03-04 18:25:17 +0800

    conditional true.false statement

---
+++ true-false.js
@@ -0,0 +1,21 @@
+//const age = 12;
+//const name = "arif khan";
+
+//let num = 25;
+//let name = null;
+
+//console.log(name);
+//let name = false;
+//let name = undefined;
+//let name = [];
+//let name = {};
+//let name = '';
+//let name = NaN;
+//let name = 'Truthy';
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

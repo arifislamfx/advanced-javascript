7e8bb2ce40881efdbe2f58f5667b8143655033ad
arifislamfx <arifislammfx@gmail.com>
2020-03-04 21:59:34 +0800

    added some code

---
+++ map-filter.js
@@ -0,0 +1,19 @@
+/*const numbers = [3, 6, 8, 9, 6];
+const output = [];
+
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+console.log(output);*/
+
+const num = [24, 50, 70, 66, 99, 67];
+const output = [];
+
+for(let i = 0; i < num.length; i++){
+    const element = num[i];
+    const result = element + element;
+    output.push(result);
+}
+console.log(output);

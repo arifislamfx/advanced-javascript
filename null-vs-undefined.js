95ecfdfec5736218de90679b97e3f72721a2e4e4
arifislamfx <arifislammfx@gmail.com>
2020-03-04 19:00:51 +0800

    null-vs-undefined clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+/*let name = null;
+console.log(name);
+let pakhi;
+console.log(pakhi);*/
+
+
+/*function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+const result = add(13, 15);
+console.log(result);*/
+
+function add (num1, num2){
+   console.log(num1 + num2);
+    return
+}
+const result = add(12, 4);
+console.log(result);
+
+let fun = undefined;
+console.log(fun);
+
+let me = null;
+console.log(me);
